1d3e3db1f337d54b37756a9011c96994b2689a6b
Maarten van Meersbergen <m.vanmeersbergen@esciencecenter.nl>
2011-02-28 12:57:56 +0000

    wipe

--- src/ibis/deploy/gui/gridvision/exceptions/OutputUnavailableException.java
+++
@@ -1,6 +0,0 @@
-package ibis.deploy.gui.gridvision.exceptions;
-
-public class OutputUnavailableException extends Exception {
-	private static final long serialVersionUID = 8427319091610532996L;
-
-}
