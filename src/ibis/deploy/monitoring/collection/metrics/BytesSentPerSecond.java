7d9b97315ec9f7ab8f630724dceab164b945fb7e
Maarten van Meersbergen <m.vanmeersbergen@esciencecenter.nl>
2011-04-14 09:44:25 +0000

    Another (hopefully definite) fix to network metrics.

--- src/ibis/deploy/monitoring/collection/metrics/BytesSentPerSecond.java
+++ src/ibis/deploy/monitoring/collection/metrics/BytesSentPerSecond.java
@@ -18,18 +18,20 @@ import ibis.deploy.monitoring.collection.exceptions.IncorrectParametersException
 public class BytesSentPerSecond extends ibis.deploy.monitoring.collection.impl.MetricDescriptionImpl implements ibis.deploy.monitoring.collection.MetricDescription {
 	private static final Logger logger = LoggerFactory.getLogger("ibis.deploy.monitoring.collection.metrics.BytesSentPerSecond");
 	
-	private static final float MAX = 1024; //1/2 GB
+	private static final float MAX = 512000; //1/2 GB
+	private long overall_max = 1024;
 	
 	public BytesSentPerSecond() {
 		super();
 		
-		name = "Bytes_Sent_Per_Sec";		
+		name = "Bytes_Sent_Per_Sec";
 		type = MetricType.LINK;
 		
 		color[0] =  64f/255f;
 		color[1] = 156f/255f;
 		color[2] = 255f/255f;
 				
+		necessaryAttributes.add(new AttributeDescription("java.lang:type=Runtime", "Uptime"));
 		necessaryAttributes.add(new AttributeDescription("ibis", "sentBytesPerIbis"));
 		
 		//outputTypes.add(MetricOutput.N);
@@ -41,42 +43,40 @@ public class BytesSentPerSecond extends ibis.deploy.monitoring.collection.impl.M
 		ibis.deploy.monitoring.collection.impl.MetricImpl castMetric = ((ibis.deploy.monitoring.collection.impl.MetricImpl)metric);
 		HashMap<IbisIdentifier, Number> result = new HashMap<IbisIdentifier, Number>();
 		HashMap<IbisIdentifier, Number> percentResult = new HashMap<IbisIdentifier, Number>();
-		long total = 0L;
 		
-		if (results[0] instanceof Map<?, ?>) {
+		//if (results[0] instanceof Long) {
+		//	long upt_elapsed	= (Long)	results[1] - (Long) castMetric.getHelperVariable("upt_prev");
+		//}
+				
+		if (results[1] instanceof Map<?, ?>) {
 			long time_now = System.currentTimeMillis();
 			long time_elapsed = time_now - (Long)castMetric.getHelperVariable(this.name+"_time_prev");					
 			castMetric.setHelperVariable(this.name+"_time_prev", time_now);
 			
-			for (Map.Entry<?,?> incoming : ((Map<?, ?>) results[0]).entrySet()) {
+			for (Map.Entry<?,?> incoming : ((Map<?, ?>) results[1]).entrySet()) {
 				if (incoming.getKey() instanceof IbisIdentifier && incoming.getValue() instanceof Long) {
 					@SuppressWarnings("unchecked") //we've just checked it!
 					Map.Entry<IbisIdentifier, Long> received = (Entry<IbisIdentifier, Long>) incoming;				
-														
+					String id = received.getKey().name();				
+					
 					if (time_elapsed != 0) {
 						float time_seconds = (float)time_elapsed / 1000.0f;
 						
 						long sent_now = received.getValue();
-						long sent = sent_now - (Long)castMetric.getHelperVariable(this.name+"_sent_prev");					
-						castMetric.setHelperVariable(this.name+"_sent_prev", sent_now);
-						
-						total += sent;
+						long sent = sent_now - (Long)castMetric.getHelperVariable(this.name+"_"+id+"_sent_prev");					
+						castMetric.setHelperVariable(this.name+"_"+id+"_sent_prev", sent_now);
 						
 						long perSec = (long) ((float)sent / time_seconds);
 						if (perSec < 0) perSec = 0;
 						result.put(received.getKey(), perSec);
 						
-						/*
-						long max_prev = (Long)castMetric.getHelperVariable(this.name+"_max_prev");
-						long max = Math.max(max_prev, perSec);
-						if (max > max_prev) {
-							castMetric.setHelperVariable(this.name+"_max_prev", max);
-						} else {
-							max = max_prev;
+						if (perSec > overall_max) {
+							overall_max = perSec;
+							System.out.println("Overall max now: " + overall_max);							
 						}
-						*/
 						
-						float percent = (float)perSec/MAX; //(float)max;
+						float percent = (float)perSec/MAX;
+						//float percent = (float)perSec/overall_max;
 						if (percent > 1f) percent = 1f;
 						
 						percentResult.put(received.getKey(), percent);
