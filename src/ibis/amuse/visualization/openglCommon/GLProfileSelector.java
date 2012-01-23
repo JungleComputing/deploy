cbc5dc4d2d58a290cf7fb6503d262f73f813dd33
Maarten van Meersbergen <m.vanmeersbergen@esciencecenter.nl>
2012-01-23 15:03:30 +0000

    - Added Logging to show available GL profiles. - Profile changed from Default to GL3 (seems to work under linux too now). - "#version 150" added to all GLSL source files and modifications made where necessary.

--- src/ibis/amuse/visualization/openglCommon/GLProfileSelector.java
+++ src/ibis/amuse/visualization/openglCommon/GLProfileSelector.java
@@ -2,8 +2,14 @@ package ibis.amuse.visualization.openglCommon;
 
 import javax.media.opengl.GLProfile;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class GLProfileSelector {
+    final static Logger logger = LoggerFactory
+            .getLogger(GLProfileSelector.class);
+
     public static final void printAvailable() {
-        System.out.println(GLProfile.glAvailabilityToString());
+        logger.info(GLProfile.glAvailabilityToString());
     }
 }
